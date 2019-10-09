56d8703c527fb79d3f3f53d539828d68a05238b9
DhouhaLou <dhouha.louhichi@esprit.tn>
2019-10-09 04:24:43 +0100

    fix

--- Server.js
+++ Server.js
@@ -3,6 +3,12 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/FoyerMap'));
+
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/FoyerMap/index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
